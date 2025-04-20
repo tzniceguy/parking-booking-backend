8e5a0ba2bd15297984416a859f6158d8787637d5
tzniceguy <joeshimbi@gmail.com>
2025-04-21 01:21:52 +0300

    feat: add model used for authentication

--- users/models.py
+++ users/models.py
@@ -1,3 +1,58 @@
+from datetime import timedelta
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+from django.contrib.auth.base_user import BaseUserManager
 
-# Create your models here.
+class PersonManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, phone_number, password=None, **extra_fields):
+        if not phone_number:
+            raise ValueError("The phone number must be set")
+        phone_number = self.normalize_email(phone_number)  # optional
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone_number, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(phone_number, password, **extra_fields)
+
+class Person(AbstractUser):
+    phone_number = models.CharField(max_length=15, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    objects = PersonManager()
+
+    def __str__(self):
+        return f"{self.first_name} - {self.last_name}"
+
+
+class Motorist(Person):
+    pass
+
+
+class OTP(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    otp_value = models.IntegerField()
+    is_used = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def is_expired(self):
+        expiry_time = self.created_at + timedelta(minutes=15)
+        return timezone.now() > expiry_time
+
+    def __str__(self):
+        return f"{self.person.phone_number} - {self.otp_value}"
