ab37e9fd86fee70fca3a3534f82a44855a2f0ab5
tzniceguy <joeshimbi@gmail.com>
2025-04-23 01:52:25 +0300

    feat:registering operator table to admin

--- users/admin.py
+++ users/admin.py
@@ -2,8 +2,9 @@
 
 from django.contrib import admin
 
-from .models import Person, Motorist, OTP
+from .models import Person, Motorist, OTP, ParkingOperator
 
 admin.site.register(Person)
 admin.site.register(Motorist)
 admin.site.register(OTP)
+admin.site.register(ParkingOperator)
