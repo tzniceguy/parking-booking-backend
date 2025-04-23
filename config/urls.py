08683b323b5e9530b5a63221575eea39ca3fcd1f
tzniceguy <joeshimbi@gmail.com>
2025-04-23 15:09:35 +0300

    feat: add endpoints to root url

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path,include
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
 urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
+    path('api/auth/', include('users.urls')),
 ]
