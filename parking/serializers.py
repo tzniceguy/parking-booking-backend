aa04e4c4a8edd78b88b8604eef6c76b9ce4dc9cd
tzniceguy <joeshimbi@gmail.com>
2025-04-24 12:27:27 +0300

    feat: serializer class for parking lot

--- parking/serializers.py
+++ parking/serializers.py
@@ -1,10 +1,13 @@
 from rest_framework import serializers
 from .models import ParkingLot
 
-class ParkingLotSerializers(serializers.ModelSerializer):
+class ParkingLotSerializer(serializers.ModelSerializer):
     operator_name = serializers.CharField(source="operator.company_name", read_only=True)
 
     class Meta:
         model = ParkingLot
         fields = "__all__"
         read_only_fields = ("id", "created_at")
+
+    def get_available_spots(self, obj):
+        return obj.spots.filter(is_available=True).count()
